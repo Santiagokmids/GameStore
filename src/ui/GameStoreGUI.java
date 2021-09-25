def323999cdd2062f9c9fda7dcdc8807d6f6742e
Gianni <70019070+G20-00@users.noreply.github.com>
2021-09-25 15:29:12 -0500

    add

--- src/ui/GameStoreGUI.java
+++ src/ui/GameStoreGUI.java
@@ -1,5 +1,50 @@
 package ui;
 
-public class GameStoreGUI {
+import java.io.IOException;
 
+import javax.swing.JOptionPane;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.control.TextField;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+
+public class GameStoreGUI {
+	  @FXML
+	    private BorderPane mainPane; 
+	@FXML
+	    private ImageView numSimulsBackground;
+
+	    @FXML
+	    private ImageView numSimulsTitle;
+
+	    @FXML
+	    private TextField numSimulsNum;
+
+	    @FXML
+	    void numSimulsContinue(ActionEvent event) {
+	    	int numSimul = Integer.parseInt(numSimulsNum.getText());
+	    	if (numSimulsNum.getText().equals("") ) {
+				JOptionPane.showMessageDialog(null, "Debe llenar los datos solicitados", "Error",
+						JOptionPane.WARNING_MESSAGE);
+			}else {
+				if(numSimul > 0) {
+					JOptionPane.showMessageDialog(null, "La simulacion debe tener una o mas repeticiones", "Error",
+							JOptionPane.WARNING_MESSAGE);
+				}
+			}
+	    }
+
+		public void loadSimuls() throws IOException {
+			FXMLLoader loader = new FXMLLoader(getClass().getResource("num-Simuls.fxml"));
+
+			loader.setController(this);
+			Parent load = loader.load();
+			mainPane.getChildren().clear();
+			mainPane.setTop(load);
+			
+		}
 }
--- src/ui/Main.java
+++ src/ui/Main.java
@@ -1,16 +1,36 @@
 package ui;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
 import javafx.stage.Stage;
 
 public class Main extends Application{
+	private GameStoreGUI gameStore;
+	
+	public Main() {
+		gameStore = new GameStoreGUI(); 
+}
+	public static void main(String [] args) {
+        launch(args);
 
-	public static void main(String[] args) {
 
-	}
-
+    }
 	@Override
-	public void start(Stage primaryStage) throws Exception {
-		
+	public void start(Stage primaryStage) throws Exception {		
+			FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main-pane.fxml"));
+			
+			fxmlLoader.setController(gameStore);
+			
+			Parent root = fxmlLoader.load();
+			
+			Scene scene = new Scene(root);
+			primaryStage.setScene(scene);
+			primaryStage.setTitle("Aliens Invaders");
+			primaryStage.setResizable(false);
+			primaryStage.show();
+			gameStore.loadSimuls();
 	}
 }
