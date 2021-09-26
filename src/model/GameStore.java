0372312248a91b26d82fb787dc7ac647283eb147
Luis Miguel <luismiguelossaarias05@gmail.com>
2021-09-26 14:42:13 -0500

    Some changes

--- src/model/GameStore.java
+++ src/model/GameStore.java
@@ -4,26 +4,22 @@ import java.lang.reflect.Array;
 import java.util.ArrayList;
 
 public class GameStore {
-
 	
 	public ArrayList<Stand>stands = new ArrayList<>();
-	
-	public ArrayList<Stand> getStands() {
-		return stands;
-	}
-
-	public void setStands(ArrayList<Stand> stands) {
-		this.stands = stands;
-	}
 	private ArrayList<Client>client;
-	public void addStand(String name,ArrayList<Game>games) {
-		
-	}
-	public void addClient(String code,String codeGame) {
-		client.add(new Client(code, codeGame));
-	}
 	
+	public GameStore() {}
+	
+	public void createStand(int numberStand) {
+		
+		for (int i = 0; i < numberStand; i++) {
+			stands.add(new Stand(null, 0));
+		}
+	}
 
+	
+	/*
+	
 	public static void insertionSort() {
 		for(int i = 1; i<(Nombre).length;i++) {
 			for(int j= i; j>0 && (Nombre)[j-1] >(Nombre)[j];j--) {
@@ -46,5 +42,22 @@ public class GameStore {
 			(Nombre)[i]= min;
 			
 		}
+	}
+	
+	*/
+	
+	public ArrayList<Stand> getStands() {
+		return stands;
+	}
+
+	public void setStands(ArrayList<Stand> stands) {
+		this.stands = stands;
+	}
+
+	public void addStand(String name,ArrayList<Game>games) {
+		
+	}
+	public void addClient(String code,String codeGame) {
+		client.add(new Client(code, codeGame));
 	}
 }
