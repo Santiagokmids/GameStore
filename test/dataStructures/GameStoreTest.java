7a8fb881f764ebd445903fb54bc4982e526395a3
Gianni <70019070+G20-00@users.noreply.github.com>
2021-09-26 19:45:35 -0500

    add

--- test/dataStructures/GameStoreTest.java
+++ test/dataStructures/GameStoreTest.java
@@ -5,13 +5,17 @@ import org.junit.jupiter.api.Test;
 
 import model.Client;
 import model.Game;
+
 class GameStoreTest {
-	private Queue <Client> queue;
-	private Stack<Game>stack;
+	private Queue<Client> queue;
+	private Stack<Game> game;
 	private HashTable<Integer, Game> hashTable;
-	
+	private QueueNode<Client> clientNode;
+
+	// queue = clientes
+	// Stack = game
 	public void setupScenary1() {
-		
+
 	}
 
 	public void setupScenary2() {
@@ -24,29 +28,26 @@ class GameStoreTest {
 
 	public void setupScenary4() {
 		queue = new Queue<>();
-		//gameStore.CreateQueue();
 	}
 
 	public void setupScenary5() {
 		queue = new Queue<>();
-		//gameStore.CreateQueue();
-		String code = "4444654"; 
-		String  codeGame = "98766543";
-		
-		String code2= "6543365";
-		String codeGame2 = "12343333";
+		String code = "4444654";
+		String codeGame = "9876,6543";
+
+		String code2 = "6543365";
+		String codeGame2 = "1234,3333";
 		Client client = new Client(code, codeGame);
-		Client client2 = new Client(code2,codeGame2);
-		QueueNode<Client>  clientNode = new QueueNode<Client>(client);
-		QueueNode<Client>  clientNode2 = new QueueNode<Client>(client2);
+		Client client2 = new Client(code2, codeGame2);
+		QueueNode<Client> clientNode = new QueueNode<Client>(client);
+		QueueNode<Client> clientNode2 = new QueueNode<Client>(client2);
 		queue.enqueue(clientNode);
 		queue.enqueue(clientNode2);
-		
+
 	}
 
 	public void setupScenary6() {
-		//gameStore = new GameStore();
-		//gameStore.CreateStack();
+		game = new Stack<>();
 	}
 
 	public void setupScenary7() {
@@ -54,38 +55,42 @@ class GameStoreTest {
 		int price = 10000;
 		int cuantity = 5;
 		String stand = "A";
-		
+
 		String code2 = "895";
 		int price2 = 13500;
 		int cuantity2 = 2;
 		String stand2 = "B";
-		
-		//gameStore.addVideoGame(code,price,cuantity,stand);
-		//gameStore.addVideoGame(code2,price2,cuantity2,stand2);
+
+		// gameStore.addVideoGame(code,price,cuantity,stand);
+		// gameStore.addVideoGame(code2,price2,cuantity2,stand2);
 	}
+
 	public void setupScenary8() {
-		//gameStore.addHastTable();
+		// gameStore.addHastTable();
 	}
+
 	@Test
 	void testCreateQueue() {
 		setupScenary1();
 		queue = new Queue<>();
 		boolean que = queue.isEmpty();
 		assertTrue(que);
-		
+
 	}
+
 	@Test
 	void testCreateStack() {
 		setupScenary2();
-		 stack = new Stack<Game>();
-		boolean stac = stack.isEmpty();
+		game = new Stack<Game>();
+		boolean stac = game.isEmpty();
 		assertTrue(stac);
-		
+
 	}
+
 	@Test
 	void testCreateHashTable() {
 		setupScenary3();
-		 hashTable = new HashTable<>(25);
+		hashTable = new HashTable<>(25);
 		boolean hash = hashTable.verifySpace();
 		assertTrue(hash);
 		assertEquals(25, hashTable.getSizeArray());
@@ -95,4 +100,45 @@ class GameStoreTest {
 		assertEquals(10, hashTable.getSizeArray());
 	}
 
+	@Test
+	void testValidateAddObjet() {
+		setupScenary4();
+		Client client = new Client("4444654", "1234,6543");
+		clientNode = new QueueNode<Client>(client);
+		queue.enqueue(clientNode);
+		assertEquals("4444654", queue.getFront().getElement().getCode());
+
+		Client client2 = new Client("6543365", "6669,33354");
+		clientNode = new QueueNode<Client>(client2);
+		queue.enqueue(clientNode);
+		assertEquals("6543365", queue.getBack().getElement().getCode());
+
+	}
+
+	@Test
+	void testRemoveObject() {
+		setupScenary5();
+		assertTrue(queue.dequeue());
+
+	}
+
+	@Test
+	void testObjectFirst() {
+		setupScenary5();
+		assertEquals("4444654", queue.getFront().getElement().getCode());
+	}
+
+	@Test
+	void testQueueIsEmpty() {
+		setupScenary4();
+		assertTrue(queue.isEmpty());
+	}
+
+	@Test
+	void testAddObjetctStack() {
+		setupScenary6();
+		Game gm = new Game (21312,1321,2,"A");
+		game.push(gm);
+		assertEquals(21312, game.top().getElement().getCode());
+	}
 }
